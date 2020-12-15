977133a062c4c2e6789b25b2e850845e67ed2335
Aslam Doctor <aslam.doctor@gmail.com>
2020-12-15 12:32:18 +0530

    added proper seeding from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\BookTableSeeder;
+use Database\Seeders\AuthorTableSeeder;
+use Database\Seeders\CategoryTableSeeder;
+use Database\Seeders\LanguageTableSeeder;
+use Database\Seeders\PublisherTableSeeder;
+use Database\Seeders\BooksLanguagesTableSeeder;
+use Database\Seeders\BooksCategoriesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +21,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $authorTableSeeder = new AuthorTableSeeder();
+        $authorTableSeeder->run();
+
+        $categoryTableSeeder = new CategoryTableSeeder();
+        $categoryTableSeeder->run();
+
+        $languageTableSeeder = new LanguageTableSeeder();
+        $languageTableSeeder->run();
+
+        $publisherTableSeeder = new PublisherTableSeeder();
+        $publisherTableSeeder->run();
+
+        $bookTableSeeder = new BookTableSeeder();
+        $bookTableSeeder->run();
+
+        $booksCategoriesTableSeeder = new BooksCategoriesTableSeeder();
+        $booksCategoriesTableSeeder->run();
+
+        $booksLanguagesTableSeeder = new BooksLanguagesTableSeeder();
+        $booksLanguagesTableSeeder->run();
+
     }
 }
